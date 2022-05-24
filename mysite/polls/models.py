db875bd73b375c15064ec6bed431066880066fd2
bouraouimed <mohamedbouraoui6@gmail.com>
2022-05-24 09:04:08 +0300

    adding visible flag

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -24,6 +24,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    is_visible = models.BooleanField(default=False)
 
     def __str__(self):
         return self.choice_text
