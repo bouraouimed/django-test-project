2eebaabea71cec5e5682810ebfe4e19be850992d
Mohamed Bouraoui <mbouraoui@rimads.ai>
2022-06-20 00:55:45 +0300

    test git actions

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,17 +83,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'github-actions',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 
 # Password validation
