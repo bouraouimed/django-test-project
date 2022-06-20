365b6b53d129574f2fcd675ee5006cbfa06cd507
Mohamed Bouraoui <mbouraoui@rimads.ai>
2022-06-20 08:50:26 +0300

    test git actions

--- mysite/tests/polls_tests.py
+++ mysite/tests/polls_tests.py
@@ -14,7 +14,7 @@ def test_was_published_recently_with_future_question():
         """
         time = timezone.now() + datetime.timedelta(minutes=30)
         future_question = Question(pub_date=time)
-        assert future_question.was_published_recently() == True
+        assert future_question.was_published_recently() == False
 
 def test_was_published_recently_with_old_question():
     """
