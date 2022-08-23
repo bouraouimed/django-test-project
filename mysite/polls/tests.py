8319f31f265608a6c79a5f86179ddd2c519f244d
Mohamed Bouraoui <mbouraoui@rimads.ai>
2022-08-23 16:18:31 +0300

    vagrant files

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -33,6 +33,7 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns True for questions whose pub_date
         is within the last day.
         """
+        print('test6')
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
