5654f14a19814f5cd7b6f933958c674431a390f2
Mohamed Bouraoui <mbouraoui@rimads.ai>
2022-08-23 17:07:31 +0300

    commit release

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,6 +15,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
+        """"
+         Test commit
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
